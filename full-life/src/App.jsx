2bdf28909ea5b36a81216ca4d25d6734f69af339
windoge102 <143863284+windoge102@users.noreply.github.com>
2025-04-18 20:33:57 +0000

    calendar

--- full-life/src/App.jsx
+++ full-life/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Calendar from "./components/Calendar.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+	<Calendar />
       <h1>Full-Life</h1>
       <p> hello! </p>
     </>
