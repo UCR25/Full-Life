1a7d3931744b5fca62a6544f59a2147e689dd310
sneha-grg <snehagrg14@gmail.com>
2025-05-02 04:23:07 -0700

    adding google

--- full-life/src/App.jsx
+++ full-life/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from './screen/landing'
+import Login from './Welcome/login'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
