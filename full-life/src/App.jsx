d71f25829a8ccc9944fe9be063794419c6aaa98a
terekj <30293049+terekj@users.noreply.github.com>
2025-05-09 11:24:33 -0700

    Add Google Login

--- full-life/src/App.jsx
+++ full-life/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from './screen/landing'
+import Signup from "./signup/signup"
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
+      <Route path="/" element={<Landing/>} />
+      <Route path="/signup" element={<Signup/>} />
       </Routes>
     </Router>
   )
--- full-life/src/main.jsx
+++ full-life/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+const CLIENT_ID = "340277123834-2sr9kanlusljq392mac22ovbql8ons86.apps.googleusercontent.com";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
     <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
