75828cd5a46d3a9b5dfc96cf38326607cad0fa4f
MatthewBrawders <122497894+MatthewBrawders@users.noreply.github.com>
2025-05-16 03:47:16 -0700

    Okay did final tests for the backend middlware connection to frontend message. This is my last commit before I delete the landing page to make sure that it doesnt cause conflcits for merging. I just wanted to test that I could still connect.

--- back-end/controllers/event_controller.py
+++ back-end/controllers/event_controller.py
@@ -111,3 +111,24 @@
 #     final_output = greeting + "\n\n" + formatted_events
 #     return JSONResponse({"html": final_output})
 
+
+#message test
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/hello")
+async def hello():
+    return {"message": "Hello from backend!"}
+
