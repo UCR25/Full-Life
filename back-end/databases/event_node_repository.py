7d12c8f092dfc08127ec36ca7adb199f71529c7c
notpandiafox <105837673+notpandiafox@users.noreply.github.com>
2025-05-16 13:12:36 -0700

    nodes added with builder

---
+++ back-end/databases/event_node_repository.py
@@ -0,0 +1,45 @@
+from category import Category
+from typing   import list
+
+class Category:
+    def __init__(self, category):
+        if not  isinstance(category, str):
+            raise TypeError("category needs to be a string")
+        self.category = category
+
+    def __eq__(self, other):
+        if not isinstance(other, str):
+            raise TypeError("other needs to be a string")
+        return self.category == other
+
+class EventNode:
+    def __init__(self, name, address, description=None, startTime=None, endTime=None):
+        self.name = name
+        self.address = address
+        self.description = description
+        self.startTime = startTime
+        self.endTime = endTime
+        self.categoriesList = []
+
+    def addToList(self, otherCategory):
+        self.categoriesList.append(otherCategory)
+
+    def __eq__(self, other):
+        return self.name == other.name and self.address == other.name
+    
+class EventNodeBuilder:
+    def __init__(self, name, address):
+        self.EventNode = EventNode(name, address)
+
+    def setDescription(self, description):
+        self.EventNode.description = description
+
+    def setStartTime(self, startTime):
+        self.EventNode.startTime = startTime
+
+    def setEndTime(self, endTime):
+        self.EventNode.endTime = endTime
+
+    def build(self):
+        return self.EventNode
+        
