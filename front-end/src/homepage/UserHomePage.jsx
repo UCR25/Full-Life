171e49daebe9e2c2d1a75ed3e1ff338221b6fc8e
sneha-grg <snehagrg14@gmail.com>
2025-05-23 01:15:54 -0700

    i forgot to git add these

--- front-end/src/homepage/UserHomePage.jsx
+++ front-end/src/homepage/UserHomePage.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserHomePage.css";
 import HomeNavbar from "../components/home_navbar";
 import TodoList from "../components/todo_list";
 import Calendar from "../components/calendar";
 import EventList from "../components/event_list";
+import SimpleProfile from "../components/navbar_info/SimpleProfile";
 
 const UserHomePage = () => {
+    const [showProfile, setShowProfile] = useState(false);
+
+    // Toggle profile visibility
+    const toggleProfile = () => {
+        setShowProfile(!showProfile);
+    };
+
     return (
-        <div>
-            <HomeNavbar />
-            <TodoList />
-            <Calendar />
-            <EventList />
+        <div className="user-home-container">
+            {/* <HomeNavbar toggleProfile={toggleProfile} /> */}
+            {!showProfile && <HomeNavbar toggleProfile={toggleProfile} />}
+            
+            {/* Show profile or main content based on state */}
+            {showProfile ? (
+                <SimpleProfile goBack={() => setShowProfile(false)} />
+            ) : (
+                <div className="main-content">
+                    <TodoList />
+                    <Calendar />
+                    <EventList />
+                </div>
+            )}
         </div>
     );
 }
