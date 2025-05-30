1c09889642aa154d0f3a8382b7ae0b7d5c93919a
terekj <30293049+terekj@users.noreply.github.com>
2025-05-30 14:38:52 -0700

    install vitest coverage

--- front-end/vitest.config.js
+++ front-end/vitest.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8', // or 'c8' if you prefer
+      reporter: ['text', 'html'],
+    },
   },
 });
